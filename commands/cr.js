eb5af7ae1eed5b984e0c06803950320f222f7970
PrinceMT <princesky334@gmail.com>
2016-05-01 15:33:36 +0530

    Added CR Command

---
+++ commands/cr.js
@@ -0,0 +1,6 @@
+ exports.commands = {
+     cr: function(arg, by, room) {
+     If (!bot.canUser('cr' by, room) return false;
+     Bot.say(by, room, ' rom.id = ' + room + ' | server.id = ' +  config.serverid + ' | server.port = ' + config.port 1 ' | server.ip = ' + config.server + '');
+},
+};
